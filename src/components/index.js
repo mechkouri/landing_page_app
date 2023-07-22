5922b0feb8e8de557ce74cf0ca11d9f419be39c1
mechkouri <saidmechkouri@gmail.com>
2023-07-22 18:38:17 +0100

    create component and navbar

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+
+import Billing from "./Billing";
+import Business from "./Business";
+import CTA from "./CTA";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+
+export {
+    Billing , Business , CTA , CardDeal , Clients , Footer , GetStarted , Hero , Stats , Testimonials , Navbar
+}
