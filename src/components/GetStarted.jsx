a295857a345ba4d79559dcbd099052886a24c5da
mechkouri <saidmechkouri@gmail.com>
2023-07-23 12:42:11 +0100

    create hero section

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import styles from "../Style"
+import { arrowUp } from "../assets"
 
 const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
+    return (
+        <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+            <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+                <div className={`${styles.flexStart} flex-row`}>
+                    <p className="font-poppins font-medium text-[18px] leading-[23px]">
+                        <div className="flex flex-row justify-between ">
+                            <span className="text-gradient">Get</span><img src={arrowUp} alt="arrowup" className="w-[23px] h-[23px] object-contain" />
+                        </div>
+                        <span className="text-gradient">started</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default GetStarted
