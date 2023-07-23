d8a1bcb4210eeeb0f0fda2056a71addd624f0b7e
mechkouri <saidmechkouri@gmail.com>
2023-07-23 18:29:05 +0100

    create CTA section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import styles from "../Style"
+import { clients } from "../constants"
+
 
 const Clients = () => {
   return (
-    <div>Clients</div>
-  )
+    <section className={`${styles.flexCenter} my-4`}>
+        <div className={`${styles.flexCenter} flex-wrap w-full`}>
+          {clients.map((client)=>(
+              <div className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`} key={client.id}>
+                <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px]  object-contain"/>
+              </div>
+          ))}
+        </div>
+    </section>
+    )
 }
 
 export default Clients
